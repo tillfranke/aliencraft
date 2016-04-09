20c8f73985d83f4787bae506d12567095063303b
tillfranke <till.franke@t-online.de>
2016-04-09 19:50:45 +0200

    asteroids and explosions

--- animations/aliencraft.py
+++ animations/aliencraft.py
@@ -2,7 +2,9 @@ import pygame
 from colors import color
 from backgrounds import Starfield
 from aliencraft.ship import Ship
-from pygame.sprite import RenderUpdates
+from aliencraft.asteroid import Asteroid
+from pygame.sprite import RenderUpdates, spritecollideany
+import numpy.random as random
 
 import time
 
@@ -14,6 +16,8 @@ SCREEN = None
 
 FPS=30
 
+START_ASTEROIDS = 5
+
 
 
 
@@ -24,32 +28,46 @@ def main():
     #SCREEN = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
     #SIZE=pygame.display.get_surface().get_size()
     SCREEN = pygame.display.set_mode(SIZE)
+
     background = Starfield(SCREEN,SIZE)
+
     ship = Ship(SIZE)
     player = RenderUpdates(ship)
-    # asteroids = RenderUpdates(rocks)
+
+    Asteroid.init(SIZE)
+    asteroids = RenderUpdates()
+    for i in range(START_ASTEROIDS):
+        asteroids.add(Asteroid())     
+    
     # aliens = RenderUpdates(coins)
     CLOCK = pygame.time.Clock()
     pygame.key.set_repeat(10,10)
-    y = 0
+    y = 0.0
 
     running = True
-    while running:
+    while running and ship.status:
         for e in  pygame.event.get():
                 if e.type == pygame.KEYDOWN:
                     if e.key == pygame.K_ESCAPE:
                         running = False
                 ship.handle_events(e)
+
         y += ship.vy
+        print(y, 10*len(asteroids.sprites()))
+        if y  > 10*len(asteroids.sprites()) and random.random() < 0.5:
+            asteroids.add(Asteroid())     
+        
         SCREEN.blit(background.render((0,y)),(0,0))
         player.update()
-        # asteroids.update()
+        asteroids.update(ship.vy)
         # aliens.update()
         player.draw(SCREEN)
-        # asteroids.draw(SCREEN)
+        asteroids.draw(SCREEN)
         # aliens.draw(SCREEN)
         pygame.display.flip()
         dt = CLOCK.tick(FPS)
+        if spritecollideany(ship, asteroids):
+            ship.crash()
     pygame.display.quit()
         
 
